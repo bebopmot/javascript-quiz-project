2f440ae8b2bca5578fa48fd401f4752bd4a6b10e
bebopmot <bebopmotinc@gmail.com>
2025-04-07 21:23:05 +0200

    quiz project day 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,42 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      // Generate a random index between 0 and the current index `i`
+      const j = Math.floor(Math.random() * (i + 1));
+      // Swap the elements at indices `i` and `j`
+      [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+    }
+    return this.questions;
+  }
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+    if (answer === currentQuestion.answer) { 
+      this.correctAnswers++;
+    }
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
 }
