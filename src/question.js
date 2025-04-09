b5e1512b7801b328a102b2412d98c2a7da6d2527
bebopmot <bebopmotinc@gmail.com>
2025-04-09 19:43:04 +0200

    Patrizia and Mohamed quiz

--- src/question.js
+++ src/question.js
@@ -1,14 +1,10 @@
 class Question {
-// YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
     constructor(text, choices, answer, difficulty) {
         this.text = text;
         this.choices = choices;
         this.answer = answer;
         this.difficulty = difficulty;
     }
-    // 2. shuffleChoices()
     shuffleChoices() {
         for (let i = this.choices.length - 1; i > 0; i--) {
             // Generate a random index between 0 and the current index `i`
--- src/quiz.js
+++ src/quiz.js
@@ -3,7 +3,7 @@ class Quiz {
   //
   // 1. constructor (questions, timeLimit, timeRemaining)
   constructor(questions, timeLimit, timeRemaining) {
-    this.questions = questions;
+    this.questions = questions; // array of instances of the class Question
     this.timeLimit = timeLimit;
     this.timeRemaining = timeRemaining;
     this.correctAnswers = 0;
@@ -24,14 +24,17 @@ class Quiz {
       // Generate a random index between 0 and the current index `i`
       const j = Math.floor(Math.random() * (i + 1));
       // Swap the elements at indices `i` and `j`
-      [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
     }
     return this.questions;
   }
   // 5. checkAnswer(answer)
   checkAnswer(answer) {
     const currentQuestion = this.questions[this.currentQuestionIndex];
-    if (answer === currentQuestion.answer) { 
+    if (answer === currentQuestion.answer) {
       this.correctAnswers++;
     }
   }
@@ -40,28 +43,21 @@ class Quiz {
     return this.currentQuestionIndex >= this.questions.length;
   }
 
+  filterQuestionsByDifficulty(difficulty) {
+    if (typeof difficulty === "number" && difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter(
+        (question) => question.difficulty === difficulty
+      );
+    }
+  }
 
-filterQuestionsByDifficulty(difficulty) {
-  if (typeof difficulty === 'number' && difficulty >= 1 && difficulty <= 3) {
-    this.questions = this.questions.filter(question => question.difficulty === difficulty);
-  };
-};
+  averageDifficulty() {
+    if (this.questions.length === 0) return 0;
 
+    const totalDifficulty = this.questions.reduce((accumulator, question) => {
+      return accumulator + question.difficulty;
+    }, 0);
 
-
-averageDifficulty() {
-  if (this.questions.length === 0) return 0;
-
-  const totalDifficulty = this.questions.reduce((accumulator, question) => {
-    return accumulator + question.difficulty;
-  }, 0);
-
-  return totalDifficulty / this.questions.length;
+    return totalDifficulty / this.questions.length;
+  }
 }
-}
-
-
-
-
-
-
